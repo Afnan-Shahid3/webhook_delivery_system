e03ee780bfc0d9174fea5115dfeb01f22852d966
Syed Afnan <afnanhfs016@gmail.com>
2026-08-26 17:45:59 +0500

    retries and failure handling

--- webhook/tasks.py
+++ webhook/tasks.py
@@ -1,17 +1,22 @@
 from celery import shared_task
+
 import requests
 
 from .models import Event, Delivery, DeliveryAttempt, Endpoint
 
-from rest_framework.response import Response
+from celery.exceptions import MaxRetriesExceededError
+
+class ServerErrorRetry(Exception):
+    pass
 
 
-@shared_task
-def send_animal_name(delivery_id):
+@shared_task(bind = True)
+def send_animal_name(self, delivery_id):
+    delay = 5 * (2 ** self.request.retries)
     delivery = Delivery.objects.get(id = delivery_id)
     try:
         response = requests.post(delivery.endpoint.url, json = delivery.event.data, timeout = 5)
-        DA = DeliveryAttempt.objects.create(delivery = delivery, response_status_code = response.status_code, attempt_number = 1, response_body = response.text)
+        DA = DeliveryAttempt.objects.create(delivery = delivery, response_status_code = response.status_code, attempt_number = self.request.retries, response_body = response.text)
         if 200 <= response.status_code < 300:
             print("You have selected an animal")
             DA.attempt_status = "SU"
@@ -26,13 +31,23 @@ def send_animal_name(delivery_id):
         else:
             DA.attempt_status = "SE"
             DA.save()
+            try:
+                raise self.retry(exc=ServerErrorRetry(f"Server returned {response.status_code}"), max_retries = 3, countdown = delay)
+            except (MaxRetriesExceededError, ServerErrorRetry):
+                delivery.delivery_status = "fail"
+                delivery.save()
+    except requests.exceptions.Timeout as time:
+        DA = DeliveryAttempt.objects.create(delivery = delivery, response_status_code = None, attempt_number = self.request.retries, attempt_status= "TO")
+        try:
+            raise self.retry(exc = time, countdown = delay, max_retries = 3)
+        except (MaxRetriesExceededError, requests.exceptions.Timeout):
             delivery.delivery_status = "fail"
             delivery.save()
-    except requests.exceptions.Timeout:
-        DA = DeliveryAttempt.objects.create(delivery = delivery, response_status_code = None, attempt_number = 1, attempt_status= "TO")
-        delivery.delivery_status = "fail"
-        delivery.save()
-    except requests.exceptions.ConnectionError:
-        DA = DeliveryAttempt.objects.create(delivery = delivery, response_status_code = None, attempt_number = 1, attempt_status= "COE")
-        delivery.delivery_status = "fail"
-        delivery.save()   
+        
+    except requests.exceptions.ConnectionError as Connection:
+        DA = DeliveryAttempt.objects.create(delivery = delivery, response_status_code = None, attempt_number = self.request.retries, attempt_status= "COE")
+        try:
+            raise self.retry(exc = Connection, countdown = delay, max_retries = 3)
+        except (MaxRetriesExceededError, requests.exceptions.ConnectionError):
+            delivery.delivery_status = "fail"
+            delivery.save()   
